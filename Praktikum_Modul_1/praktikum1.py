ab62e261acdd7165b4e3d574f6b310367307e9b8
tegar iman <44028234+tegar12@users.noreply.github.com>
2019-06-17 17:42:15 +0700

    Add files via upload

---
+++ Praktikum_Modul_1/praktikum1.py
@@ -0,0 +1,13 @@
+karakter = 'Jon Snow'
+
+price = 10
+x = 7
+y = 8
+z = 2
+
+print('karakter = ',karakter)
+print('Price = ',price)
+print('x = ',x)
+print('y = ',y)
+print('z = ',z)
+print('((x*y)**z)/y = ',((x*y)**z)/y)
